4ffb0760d7634e930b8e5bc0bfbd16a31aacb243
indyuriii <indyuriii08@gmail.com>
2025-03-10 11:37:16 +0800

    calo beach

---
+++ project-title/app-files/data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-p1",
+      "name": "p1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "yaw": -0.30816133118445954,
+        "pitch": -0.036650441330241534,
+        "fov": 1.4308333190114557
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5444449638024533,
+          "pitch": -0.0020009573323989116,
+          "rotation": 0,
+          "target": "1-p2"
+        },
+        {
+          "yaw": 2.0035287339129226,
+          "pitch": -0.10015347930232466,
+          "rotation": 0,
+          "target": "2-p3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.30816131954336257,
+          "pitch": -0.03665042909235794,
+          "title": "Calo Beach",
+          "text": "<div _ngcontent-ng-c695106026=\"\" class=\"markdown markdown-main-panel stronger\" id=\"model-response-message-contentr_d89ee3e2b2f528bb\" dir=\"ltr\" style=\"--animation-duration: 400ms; --fade-animation-function: linear; animation: 0s ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px none rgb(27, 28, 29); inset: auto; clear: none; clip: auto; color: rgb(27, 28, 29); columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; fill: rgb(0, 0, 0); filter: none; flex-shrink: 1; float: none; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-size-adjust: none; font-kerning: auto; font-optical-sizing: auto; font-feature-settings: normal; font-variation-settings: normal; font-variant-position: normal; font-variant-emoji: normal; font-stretch: normal; font-size: 16px; line-height: 28px; font-family: &quot;Google Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif; gap: normal; hyphens: manual; isolation: auto; margin: 0px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(27, 28, 29) none 0px; overlay: none; padding: 0px; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1;\"><p data-sourcepos=\"1:1-1:81\" style=\"animation: 0s ease 0s 1 normal none running none; appearance: none; background: none 0% 0% / auto repeat scroll padding-box border-box rgba(0, 0, 0, 0); border: 0px none rgb(27, 28, 29); inset: auto; clear: none; clip: auto; columns: auto; contain: none; container: none; content: normal; cursor: auto; cx: 0px; cy: 0px; d: none; direction: ltr; fill: rgb(0, 0, 0); filter: none; flex-shrink: 1; float: none; font: 16px / 28px &quot;Google Sans&quot;, &quot;Helvetica Neue&quot;, sans-serif; gap: normal; hyphens: manual; isolation: auto; margin-bottom: 16px; marker: none; mask: none; offset: normal; opacity: 1; order: 0; outline: rgb(27, 28, 29) none 0px; overlay: none; page: auto; perspective: none; position: static; quotes: auto; r: 0px; resize: none; rotate: none; rx: auto; ry: auto; scale: none; speak: normal; stroke: none; transform: none; transition: all; translate: none; visibility: visible; x: 0px; y: 0px; zoom: 1;\">Calo Beach in Cabadbaran offers a peaceful, golden-sand shoreline for relaxation.</p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-p2",
+      "name": "p2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.226346668776575,
+          "pitch": 0.06929134097538636,
+          "rotation": 0,
+          "target": "0-p1"
+        },
+        {
+          "yaw": 1.3287545520470871,
+          "pitch": -0.007615901303093864,
+          "rotation": 0,
+          "target": "2-p3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-p3",
+      "name": "p3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 896,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3298846107922664,
+          "pitch": 0.06331155142734701,
+          "rotation": 0,
+          "target": "0-p1"
+        },
+        {
+          "yaw": 0.664772389528073,
+          "pitch": 0.1042160005993793,
+          "rotation": 0,
+          "target": "1-p2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
